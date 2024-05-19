9882861e22d9f13b0c9faa37068d4ef79d1ba946
Pedro Henrique Vieira Ribeiro <pedrohvribeiro@gmail.com>
2024-05-19 18:48:17 -0300

    add: config cors

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('vans')
     .build();
+  app.enableCors();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
