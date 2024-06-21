083ae019483d9b10dc36830734e62f80e4d67e6e
Pedro Henrique Vieira Ribeiro <79656858+PedroHenriquevr@users.noreply.github.com>
2024-06-21 20:19:15 -0300

    fix: error module veiculo

--- src/veiculo/veiculo.module.ts
+++ src/veiculo/veiculo.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { VeiculoService } from './veiculo.service';
 import { VeiculoController } from './veiculo.controller';
+import { UploadModule } from 'src/upload/upload.module';
 
 @Module({
+  imports: [UploadModule],
   providers: [VeiculoService],
   controllers: [VeiculoController],
 })
